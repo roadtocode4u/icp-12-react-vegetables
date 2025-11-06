c0a033b3334b47957ceafa428243ea82282ca0e7
Suraj Shende <surajshende247@gmail.com>
2025-11-06 19:37:28 +0530

    feat: add Button component and integrate into Home view

---
+++ src/components/Button.jsx
@@ -0,0 +1,30 @@
+const BUTTON_VARIANTS = {
+  primary: "bg-[#FFBC4C] hover:bg-[#e6a200] transition-duration-700",
+  secondary: "bg-blue-500 text-white hover:bg-blue-600 transition-duration-700",
+  tertiary:
+    "bg-slate-700 text-white hover:bg-slate-800 transition-duration-700",
+};
+
+const BUTTON_SIZES = {
+  large: "text-3xl px-8 py-1.5 m-3",
+  medium: "text-2xl px-8 py-1 m-2",
+  small: "text-xl px-6 py-0.5 m-1",
+};
+
+function Button({
+  title,
+  onClick = () => {},
+  variant = "primary",
+  size = "medium",
+}) {
+  return (
+    <button
+      onClick={onClick}
+      className={`rounded-full ${BUTTON_VARIANTS[variant]} ${BUTTON_SIZES[size]}  cursor-pointer`}
+    >
+      {title}
+    </button>
+  );
+}
+
+export default Button;
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,5 +1,6 @@
 import FeatureSection from "../components/FeatureSection";
 import ImgMainHeader from "./../assets/main-header.png";
+import Button from "./../components/Button";
 
 import Footer from "./../components/Footer";
 import Navbar from "./../components/Navbar";
@@ -21,6 +22,12 @@ function Home() {
             <h1 className="text-[40px]! text-center md:text-[70px]! text-[#e69100]">
               One platform for all the vegetables you need.
             </h1>
+
+            <div className="flex flex-col md:flex-row gap-4 mt-8 justify-center">
+              <Button title={"Explore Now"} variant="primary" size="medium" />
+
+              <Button title={"Call Us"} variant="tertiary" size="medium" />
+            </div>
           </div>
         </div>
 
